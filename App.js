68343e03d8baeec378adcfd88f4f9e5b4be99109
psaiaditya <96437894+psaiaditya@users.noreply.github.com>
2022-03-24 08:12:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,89 @@
-import React,{useState} from "react";
-import TodoList from "./TodoList";
+import React, { useEffect, useState } from "react";
 import "./index.css";
+import Form from "./Form";
 
-function App(){
-const [task,setTask]=useState("");
-const [todos,setTodos]=useState([]);
-const changeHandler =e=>{
-  setTask(e.target.value)
+//to get data from local storage
+
+const getLocal=()=>{
+  let todo=localStorage.getItem('lists');
+  if(todo){
+    return JSON.parse(localStorage.getItem('lists'));
+  }
+  else{
+    return [];
+  }
 }
-const submitHandler= e=>{
-  e.preventDefault();
-  const newTodos=[...todos,task];
-  setTodos(newTodos);
-  setTask("");
-}
-const deleteHandler= (indexValue)=>{
-  const newTodos=todos.filter((todo,index)=>index!==indexValue);
-  setTodos(newTodos);
-}
-  return(
-    <div className="card">
-      <div>&nbsp;
-        <h1>Todo Application</h1>&nbsp;
-        <form onSubmit={submitHandler}>
-        <input type="text" name="task" value={task} onChange={changeHandler}/>&nbsp;&nbsp;
-        <button>Add</button>
-        </form>
-      </div>
-      <TodoList todolist={todos} deleteHandler={deleteHandler}/>
+
+function App() {
+
+
+  const [task, setTask] = useState("");
+
+  const [todos, setTodos] = useState(getLocal());
+
+  const [date,setDate]=useState(new Date().getDate());
+
+  const [validate,setValidate]=useState("");
+
+  function changeHandler(e) {
+
+    setTask(e.target.value)
+  }
+  function dateHandler(e){
+    setDate(e.target.value)
+  }
+  //
+  function submitHandler(e) {
+
+    e.preventDefault();
+
+    const newTodos = [...todos,{task,date}]
+
+    setTodos(newTodos);
+    setTask("");
+    setDate("");
+
+
+  }
+
+
+  function deleteHandler(indexValue) {
+    const del = todos.filter((todo, index) => index !== indexValue)
+    setTodos(del)
+  }
+  // add data to local storage
+  useEffect(()=>{
+   localStorage.setItem('lists',JSON.stringify(todos))
+  },[todos])
+  return (
+    <div className="App">
+       &nbsp;&nbsp;
+        <div className="input1"> 
+          <input type="text" value={task} onChange={(e) => {
+          changeHandler(e)
+        }} />&nbsp;&nbsp;
+        <input type="date" value={date} onChange={(e)=>{
+           dateHandler(e)
+        }}/>&nbsp;
+        <button onClick={(e) => {
+        if(task.length!=""){
+          submitHandler(e)
+        }
+       
+        else{
+           setValidate("Please Enter Task!");
+        }
+        
+        
+      }}>Add</button>
+        </div>
+        
+      
+      &nbsp;
+      <Form todos={todos} deleteHandler={deleteHandler} />
+      {validate}
     </div>
+    
   )
 }
-
-export default App;
+export default App
